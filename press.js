36ffe6c299b78a39086af723c36b3cfd51a77f71
HyGlobalHD <GamerXzavier@gmail.com>
2018-01-06 00:03:45 +0800

    Create press.js

---
+++ press.js
@@ -0,0 +1,29 @@
+
+// Still In Development
+
+
+// get input(?)
+var keys = {
+ sound_1: ['1'],
+ sound_2: ['2],
+ sound_4: ['4'],
+ sound_5: ['5'],
+ sound_6: ['6'],
+ sound_7: ['7'],
+ sound_8: ['8'],
+ sound_9: ['9'],
+ sound_0: ['0'],
+ sound_10: ['-'],
+ sound_11: ['='],
+ sound_12: ['`'],
+ sound_13: ['q'],
+ sound_14: ['w'],
+ sound_15: ['e'],
+};
+
+// load sound pre-load
+
+var preloaders = [
+ //sound to do
+];
+
