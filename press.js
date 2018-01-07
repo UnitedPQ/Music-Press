db21a4478dd96338c11bc29337645598610db6a4
HyGlobalHD <GamerXzavier@gmail.com>
2018-01-07 12:28:52 +0800

    Update press.js

--- press.js
+++ press.js
@@ -4,26 +4,39 @@
 
 // get input(?)
 var keys = {
- sound_1: ['1'],
- sound_2: ['2],
- sound_4: ['4'],
- sound_5: ['5'],
- sound_6: ['6'],
- sound_7: ['7'],
- sound_8: ['8'],
- sound_9: ['9'],
- sound_0: ['0'],
- sound_10: ['-'],
- sound_11: ['='],
- sound_12: ['`'],
- sound_13: ['q'],
- sound_14: ['w'],
- sound_15: ['e'],
+ sound_1: ['1', '!'],
+ sound_2: ['2', '@'],
+ sound_3: ['3', '#'],
+ sound_4: ['4', '$'],
+ sound_5: ['5', '%'],
+ sound_6: ['6', '^'],
+ sound_7: ['7', '&'],
+ sound_8: ['8', '*'],
+ sound_9: ['9'. '('],
+ sound_0: ['0', ')'],
+ sound_10: ['-', '_'],
+ sound_11: ['='. '+'],
+ sound_12: ['`', '~'],
+ sound_13: ['q', 'Q'],
+ sound_14: ['w', 'W'],
+ sound_15: ['e', 'E'],
+ sound_16: ['r', 'R'],
+ sound_17: ['t', 'T'],
+ sound_18: ['y', 'Y'],
+ sound_19: ['u', 'U'],
+ sound_20: ['i', 'I'],
+ sound_21: ['o', 'O'],
+ sound_22: ['p', 'P'],
+ sound_23: ['[', '{'],
+ sound_24: [']', '}'],
+ sound_25: ['\', '|'],
 };
 
 // load sound pre-load
 
 var preloaders = [
- //sound to do
+ //sound todo
 ];
+ 
+ 
 
